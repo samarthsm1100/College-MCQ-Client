5f3b4b22ef5c3d9360ccbff1762f51c8ce21a725
adityamore7000 <adi.more7000@gmail.com>
2024-01-17 12:16:22 +0530

    refactor nextui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 "use client";
-import Image from 'next/image'
-import { Button, NextUIProvider } from '@nextui-org/react'
+import { Button } from '@nextui-org/react'
 export default function Home() {
   return (
-    <NextUIProvider>
       <Button color='danger'>
         Button
       </Button>
-    </NextUIProvider>
   )
 }
