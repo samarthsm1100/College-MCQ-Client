af535910440e9336aa36632d60370d72155ad1d6
Samarth <samarthmali1100@gmail.com>
2024-04-11 23:23:54 +0530

    client side context is saved to localStorage

--- src/app/tests/[test_id]/layout.tsx
+++ src/app/tests/[test_id]/layout.tsx
@@ -1,8 +1,20 @@
+'use client'
+
 import QuestionsList from "@/components/QuestionsList";
 import { questions } from "../../../../data/questions";
 import Timer from "@/components/Timer";
 import { Button } from "@nextui-org/react";
+// import { useRouter } from "next/navigation";
+// import { useResults } from "@/contexts/ResultsContext";
+
 export default function Layout({children,params}:{children:React.ReactNode,params:{test_id:string}}) {
+
+  const handleSubmit = async () => {
+    localStorage.removeItem('results');
+    window.location.href = '/';
+    // localStorage.setItem('isInitialized',JSON.stringify(false));
+  }
+  
   return (
     <>
         <div>
@@ -10,7 +22,7 @@ export default function Layout({children,params}:{children:React.ReactNode,param
             <h1 className="text-2xl font-bold">MCQ <span className="text-blue-500">Test</span></h1>
             <div className="flex gap-2 items-center">
           <Timer hours={3} minutes={0} seconds={0}/>
-            <Button color="success" className="font-semibold text-white text-md">Submit</Button>
+            <Button color="success" className="font-semibold text-white text-md" onClick={handleSubmit}>Submit</Button>
             </div>
           </nav>
           <section className="flex ">
